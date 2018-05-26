972b5e4fb1adf6269b65f056695907ce68d4186a
aaaalhaj000 <39376823+aaaalhaj000@users.noreply.github.com>
2018-05-26 17:19:30 +0300

    تطبيق لحساب القيمة المضافة

---
+++ VAT.py
@@ -0,0 +1,19 @@
+def main():
+    x=input("Total Purchases=SR  ")
+
+    TAX=float(x*5/100)
+    VAT=TAX+x
+    print("Total amount SR {}").format(VAT)
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':main()
